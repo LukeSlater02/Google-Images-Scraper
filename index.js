48ad37a5cb84fc543a41b2cdec449cebea85eb20
LukeSlater02 <lukeslater101@gmail.com>
2022-09-17 16:01:22 -0500

    search google and navigate to the images section

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
 
-async function start(){
+async function start(searchString) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto("https://www.google.com/search?q=apple&tbm=isch&sxsrf=ALiCzsbnf3F8a_gCTI1LLwEXtX9RBxC61w%3A1663444308224&source=hp&biw=1278&bih=959&ei=VCUmY4j_B7LXkPIPiZ256Aw&iflsig=AJiK0e8AAAAAYyYzZN--FRBjenNypbH-AFHYmeiB-zNl&ved=0ahUKEwiI-dv4zJz6AhWyK0QIHYlODs0Q4dUDCAc&uact=5&oq=apple&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIECAAQAzIICAAQgAQQsQMyCwgAEIAEELEDEIMBMgsIABCABBCxAxCDATIFCAAQgAQyBQgAEIAEMgsIABCABBCxAxCDAToHCCMQ6gIQJzoECCMQJzoICAAQsQMQgwFQ9xRYrhpg_hpoAXAAeACAAUyIAewCkgEBNZgBAKABAaoBC2d3cy13aXotaW1nsAEK&sclient=img")
-    
-    await page.screenshot({path: "ss.png"})
+    await page.goto(`https://www.google.com/search?q=${searchString}&sxsrf=ALiCzsYVk_Rwn0zrwBqz6oiN5EaRzdYr3g%3A1663448119014&source=hp&ei=NjQmY66fOLngkPIPwLu-8AQ&iflsig=AJiK0e8AAAAAYyZCR0FlER0NZYY98nhL69yOAkUS4U4j&ved=0ahUKEwiukuyR25z6AhU5MEQIHcCdD04Q4dUDCAk&uact=5&oq=${searchString}&gs_lcp=Cgdnd3Mtd2l6EAMyCwguEIAEELEDENQCMgsILhCABBCxAxDUAjIICC4QgAQQsQMyBQgAEIAEMgsILhCABBCxAxDUAjIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyBAgAEEM6BAgjECc6CwgAEIAEELEDEIMBOhEILhCABBCxAxCDARDHARDRAzoLCC4QgAQQsQMQgwE6BQgAEJECOgcILhDUAhBDOggIABCxAxCDAToICAAQgAQQyQM6BQgAEJIDOgoILhCxAxDUAhBDOggILhCxAxDUAjoLCC4QgAQQxwEQrwFQAFi1B2DOCGgAcAB4AIABywGIAb8LkgEFMC45LjGYAQCgAQE&sclient=gws-wiz`)
+
+    // const names = await page.evaluate(() => {
+    //     return Array.from(document.querySelectorAll(".OztcRd")).map(title => title.textContent)
+    // })
+    // await fs.writeFile("scrapedImageTitles.txt", names.join("\r\n"))
+
+    //uses an html class to select all html elements and format them into an array and then passes that data into the function
+    // const images = await page.$$eval(".hdtb-mitem a", (sectionAnchor) => {
+    //     return sectionAnchor.map(section => section.href)
+    // })
+
+    let imagesSectionUrl = await page.evaluate(() => {
+        let searchSections = Array.from(document.querySelectorAll(".hdtb-mitem a")).map(anchor => anchor.href)
+        return searchSections.find(e => e.includes("&tbm=isch"))
+    })
+    await page.goto(imagesSectionUrl)
+    await page.screenshot({ path: "images.png" })
+
+
+    // for(let image of imageUrls){
+
+    //     // const imagePage = await page.goto(image)
+    //     // await fs.writeFile(image.split("/").pop(), await imagePage.buffer())
+
+    // }
 
     await browser.close()
-    
+
 }
 
-start()
+start("banana")
