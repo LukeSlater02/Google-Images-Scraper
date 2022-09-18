f0cd1b6cea7f445bd9b11a6b24bacdf25973023c
LukeSlater02 <lukeslater101@gmail.com>
2022-09-18 14:31:01 -0500

    working on image download and resizing

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const puppeteer = require('puppeteer')
-const fs = require('fs/promises')
-const client = require('https')
+var fs = require('fs')
+const Axios = require('axios')
+const path = require('path')
+const sharp = require('sharp')
+
+// async function resize(imageName){
+//     sharp(`images/${imageName}.jpg`).resize(128, 128).toFile(`images/${imageName}_thumbnail.jpg`)
+// }
+
+async function download(url, imageName) {
+    const downloadPath = path.resolve(__dirname, 'images', `${imageName}.jpg`)
+
+    const response = await Axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+    })
+
+    Axios.get(url, { responseType: 'arraybuffer' }).then((res) => {
+        return sharp(res.data)
+            .resize(128, 128, {
+                fit: 'inside',
+            })
+            .toFile(`images/${imageName}.jpg`)
+    })
+
+    //response.data.pipe(fs.createWriteStream(downloadPath))
+
+    // return new Promise((resolve, reject) => {
+    //     response.data.on('end', () => {
+    //         sharp(`images/${imageName}.jpg`).resize(128, 128, {
+    //             fit: 'inside',
+    //         }).jpeg().toFile(`images/${imageName}_thumbnail.jpg`).then(() => resolve())
+    //     })
+    //     response.data.on('error', err => {
+    //         reject(err)
+    //     })
+    // })
+}
 
 async function start(searchString) {
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(`https://www.google.com/search?q=${searchString}&sxsrf=ALiCzsYVk_Rwn0zrwBqz6oiN5EaRzdYr3g%3A1663448119014&source=hp&ei=NjQmY66fOLngkPIPwLu-8AQ&iflsig=AJiK0e8AAAAAYyZCR0FlER0NZYY98nhL69yOAkUS4U4j&ved=0ahUKEwiukuyR25z6AhU5MEQIHcCdD04Q4dUDCAk&uact=5&oq=${searchString}&gs_lcp=Cgdnd3Mtd2l6EAMyCwguEIAEELEDENQCMgsILhCABBCxAxDUAjIICC4QgAQQsQMyBQgAEIAEMgsILhCABBCxAxDUAjIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyBAgAEEM6BAgjECc6CwgAEIAEELEDEIMBOhEILhCABBCxAxCDARDHARDRAzoLCC4QgAQQsQMQgwE6BQgAEJECOgcILhDUAhBDOggIABCxAxCDAToICAAQgAQQyQM6BQgAEJIDOgoILhCxAxDUAhBDOggILhCxAxDUAjoLCC4QgAQQxwEQrwFQAFi1B2DOCGgAcAB4AIABywGIAb8LkgEFMC45LjGYAQCgAQE&sclient=gws-wiz`)
+    await page.goto(`https://www.google.com/search?q=${searchString}`)
 
     // const names = await page.evaluate(() => {
     //     return Array.from(document.querySelectorAll(".OztcRd")).map(title => title.textContent)
@@ -39,7 +77,12 @@ async function start(searchString) {
         let imageUrl = await page.evaluate(() => {
             return document.querySelector('.n3VNCb.KAlRDb').src
         })
-        page.goBack()
+        let imageName = await page.evaluate(() => {
+            return document.querySelector('.n3VNCb.KAlRDb').alt.replaceAll(/[^a-zA-Z0-9]/g, '_');
+        })
+        await download(imageUrl, imageName)
+        //await resize(imageName)
+        await page.goBack()
     }
 
 
@@ -47,4 +90,4 @@ async function start(searchString) {
 
 }
 
-start("watermelon")
+start("master chief")
